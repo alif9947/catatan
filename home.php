3e9bab6ec08fa483915f7e58b24525ee216f27fd
alif <madbyhe@gmail.com>
2024-08-29 07:45:27 +0700

    update

--- home.php
+++ home.php
@@ -2,14 +2,13 @@ php
 copy code 
 <?php
 function sendMessage($number, $massage){
-    $apiUrl =
-    'https://api.whatsapp.com/send?phone='.$number.'&text='.urlcode($massage);
+    $apiUrl = 'https://api.whatsapp.com/send?phone='.$number.'&text='.urlcode($massage);
     $response = file_get_contents($apiUrl);
     return $response;
-
 }
 
 $number = '628123456789';
 $massage = 'selamat datang di layanan kami ';
 sendMessage($number, $massage);
 ?>
+
